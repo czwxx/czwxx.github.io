3d3c6fd71d0f8a1d32c3e20fb75110299475e410
czwxx <156472779+czwxx@users.noreply.github.com>
2024-01-14 20:43:23 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// bio site by @trvnj
+
+$("#discord").click(function (){
+    navigator.clipboard.writeText("trvnj");
+    $("#notification").fadeIn();
+})
+
+$("#x").click(function() {
+    $("#notification").fadeOut();
+})
+
+$("#steam").click(function() {
+    window.open("https://steamcommunity.com/id/trvnj/");
+})
+
+// bio site by @trvnj
